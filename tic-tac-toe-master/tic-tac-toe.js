263615b54a00b0c09e379e68f692991baa8443f7
Daniela Spencer <danielaspencer9@gmail.com>
2025-10-16 16:47:52 -0400

    Exercise 3

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,20 +1,28 @@
 window.addEventListener('DOMContentLoaded', () => {
   const board = document.getElementById('board');
-  if (!board) return;
+  const box = board.querySelectorAll("#board > div");
 
-const box = board.querySelectorAll("div");
-box.forEach(box => box.classList.add("square"));
-
-  let currentPlayer = 'X';
-const state = Array(9).fill(null);
-
-box.forEach((box, i) => {
-  box.textContent = 'currentPlayer';
-  box.classList.remove('X', 'O');
-  box.classList.add('currentPlayer');
+  box.forEach(box => box.classList.add("square"));
   
-  state[i] = currentPlayer;
-  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  let currentPlayer = 'X';
+  const state = Array(9).fill(null);
+
+  box.forEach ((box, index) => {
+    box.addEventListener('mouseenter', () => {
+      if (!state[index]) box.classList.add("hover");
+    });
+    box.addEventListener('mouseleave', () => {
+      box.classList.remove("hover");
+    });
+
+    box.addEventListener('click', () => {
+      if (!state[index]) return;
+
+      box.textContent = currentPlayer;
+      box.classList.remove("X","O","hover");
+      box.classList.add(currentPlayer);
+      state[index] = currentPlayer;
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    });
   });
 });
-  
